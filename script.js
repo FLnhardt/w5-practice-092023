0d417f71e65d6e24962567ab6f0ab52629efbe96
ricsimarta <ricsimarta@gmail.com>
2023-10-25 10:23:16 +0200

    break

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ console.log(rootElement)
 
 rootElement.innerHTML = "sziasztok"
 
-async function init() {
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    const data = await response.json()
+const countryHtml = (countryName, countryPop) => `
+    <h2>country name: ${countryName}</h2>
+    <h3>country pop: ${countryPop}</h3>
+`
 
-    data.map(country => console.log(country.name.common))
+async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
+    const response = await fetch("https://restcountries.com/v3.1/all") // megvárjuk a fetch válaszát (promise helyett)
+    const data = await response.json() // megvárjuk, hogy a response megjöjjön (promise helyett) -> itt lesz elérhető az adatunk
+    console.log(data)
+
+    data.forEach(country => console.log(countryHtml(country.name.common, country.population)))
 }
 
 init()
